45fd47f70f842c50ff6f5a374b9fb8bca1b1c6d9
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-24 16:35:01 +0900

    .

--- commentf.js
+++ commentf.js
@@ -1,6 +1,10 @@
+function submitForm() {
+    document.getElementById("comment-form").submit();
+}
+
 var onloadCallback = function () {
     hcaptcha.render('captchabox', {
         'sitekey': '9d5069dc-c77a-4fcf-a3f8-a2fa3bfa90b6',
-        'callback': onSignin
+        'callback': submitForm
     });
 };
--- worker/runkit.js
+++ worker/runkit.js
@@ -1,17 +1,20 @@
 const express = require('express');
 let faunadb = require('faunadb')
 const { Octokit } = require("@octokit/core")
-const { axios } = require("axios");
+const { axios } = require("axios"); var bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const octokit = new Octokit({});
 
-const app = express()
 let q = faunadb.query
 var client = new faunadb.Client({ secret: process.env.FUNNA_API_KEY })
 axios
 
-app.get('/repos/:user', async function (req, res) {
-    res.send(data.data);
+app.post('/post', function (request, response) {
+    console.log(request.body) //you will get your data in this as object.
+    response.send(request.body)
 })
 
 
